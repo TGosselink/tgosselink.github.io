274eadb8eeddbe6f403f6432df6bcfc5c89c059e
TGosselink <theo@gosselink.net>
2018-05-08 18:59:28 +0200

    Redux 2

--- robofriend/src/containers/App.js
+++ robofriend/src/containers/App.js
@@ -1,15 +1,29 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
+import { setSearchField } from '../Actions';
+
+const mapstateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: [],
-            searchfield: ''        
+            robots: []      
         }
     }
 
@@ -19,14 +33,11 @@ class App extends Component {
             .then(users => this.setState({ robots: users }));
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    }
-
     render() {
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
 
         return !robots.length ?
@@ -34,7 +45,7 @@ class App extends Component {
             (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <CardList robots={filteredRobots}/>
                     </Scroll>
@@ -43,4 +54,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapstateToProps, mapDispatchToProps)(App);
