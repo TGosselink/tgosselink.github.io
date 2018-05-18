695b3a8bcfc52cd542fa3856bdcd176c7a64f9d0
TGosselink <theo@gosselink.net>
2018-05-18 15:16:27 +0200

    redux met Middleware

--- robofriend/src/index.js
+++ robofriend/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './Reducers';
+import { searchRobots, requestRobots } from './Reducers';
 import 'tachyons';
 
 const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+
 //const store = createStore(searchRobots, applyMiddleware(logger))
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(searchRobots, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware, logger)));
 
 ReactDOM.render(
         <Provider store={store}>
